ba69b6b09c0f40c9fe44037a3414078fc7cc34bb
Tyler Gandy <tylerbgandy@gmail.com>
2022-01-29 22:57:17 -0500

    updated changes for custom cursor pointer

---
+++ javascript/index.js
@@ -0,0 +1,43 @@
+let menu = document.getElementById('menuSelect')
+let header = document.querySelector('header');
+let cursorOne= document.querySelector('.cursorOne')
+let cursorTwo= document.querySelector('.cursorTwo')
+let links = document.querySelectorAll('a')
+
+// when the menu button in upper right corner is pressed
+// the header will toggle from displayed to hidden when the page
+// is benath a certain width
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+
+})
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times')
+    header.classList.remove('active')
+}
+
+// adds functionality to the cursor to porivde custom cursor response. 
+
+window.onmousemove = (e) =>{
+cursorOne.style.top = e.pageY + 'px';
+cursorOne.style.left = e.pageX + 'px';
+cursorTwo.style.top = e.pageY + 'px';
+cursorTwo.style.left = e.pageX + 'px';
+}
+
+changeCursor = () =>{
+    links.forEach(links =>{
+        links.onmouseenter = () => {
+        cursorOne.classList.add('active');
+        cursorTwo.classList.add('active')
+        }
+        links.onmouseleave = () =>{
+            cursorOne.classList.remove('active')
+            cursorTwo.classList.remove('active')
+        }
+    })
+}
+
+changeCursor();
